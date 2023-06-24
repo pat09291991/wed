451aaea19fce73f39f4dd6705159f3cba0ddf66a
Sheryl Broqueza <GAC278@ad.zalora-ops.com>
2023-06-24 19:05:37 +0800

    she: added file for invitesection

--- src/Pages/Home/Homepage.jsx
+++ src/Pages/Home/Homepage.jsx
@@ -6,6 +6,7 @@ import PatShe2 from '../../assets/images/patShe2.png'
 import '../../she.css'
 import { Link } from 'react-scroll';
 import SnowflakeAnimation from "../../component/SnowflakeAnimation"
+import { InviteSectionPage } from "./InviteSectionPage"
 
 export const Homepage = () => {
     const [loading, setLoading] = useState(true)
@@ -142,41 +143,7 @@ export const Homepage = () => {
                         </Row>
                     </section>
                     <section id="inviteSection" style={{ transform: 'translate3d(0px, 0.04px, 0px)' }}>
-                        <div className="vh-100 pt-4" id="h-s1">
-                            <div id="h-s1-0">
-                                <div className="entitled-w">
-                                    <div className="entitled" style={{ transform: 'translate3d(0px, 0%, 0px) rotateX(0deg)' }} >THE DATE</div>
-                                </div>
-                                <div className="line" style={{ transform: 'scaleX(1)' }} ></div>
-                                <h2 className="title-1"><span style={{ transform: 'translate3d(0px, 0%, 0px) rotateX(0deg)' }} >Thursday</span> <span
-                                    style={{ transform: 'translate3d(0px, 0%, 0px) rotateX(0deg)' }} >December 21</span> <span
-                                        style={{ transform: 'translate3d(0px, 0%, 0px) rotateX(0deg)' }} >2023</span></h2>
-                            </div>
-                            <div id="h-s1-1">
-                                <div className="entitled-w">
-                                    <div className="entitled" style={{ transform: 'translate3d(0px, 0%, 0px) rotateX(0deg)' }} >THE INVITE</div>
-                                </div>
-                                <div className="line" style={{ transform: 'scaleX(1)' }} ></div>
-                                <p className="txt-1" style={{ transform: 'translate3d(0px, 0%, 0px); opacity: 1' }} >Join the celebration as Alice Veley
-                                    and Gene Ross say "I do" on the shores of beautiful Lake Tahoe, California.</p><a className="txt-0 _tb"
-                                        href="https://veleyross.app.rsvpify.com" style={{ transform: 'translate3d(0px, 0%, 0px); opacity: 1' }} >RSVP<span
-                                            className="under"></span></a>
-                            </div>
-                            <ul>
-                                <li id="h-s1-no-0" className="h-s1-no" style={{ transform: 'translate3d(0px, -44.0352px, 0px)' }} >12</li>
-                                <li id="h-s1-no-1" className="h-s1-no" style={{ transform: 'translate3d(0px, 14.6784px, 0px)' }} >21</li>
-                                <li id="h-s1-no-2" className="h-s1-no" style={{ transform: 'translate3d(0px, 88.0704px, 0px)' }} >23</li>
-                            </ul>
-                            <ul id="h-s1-caption-w">
-                                <li id="h-s1-caption-0" className="txt-2" style={{ transform: 'transform: translate3d(0px, -73.392px, 0px)' }} ><span>Sep 8,
-                                    2019</span><span>She said he was crazy.</span></li>
-                                <li id="h-s1-caption-1" className="txt-2" style={{ transform: 'transform: translate3d(0px, 29.3568px, 0px)' }} ><span>Lake
-                                    Louise</span><span>He asked her to marry him.</span></li>
-                                <li id="h-s1-caption-2" className="txt-2" style={{ transform: 'translate3d(0px, 58.7136px, 0px)' }} >
-                                    <span>3:40PM</span><span>She said yes.</span></li>
-                                <li id="h-s1-caption-3" className="txt-2"><span>Oct 14, 2019</span><span>We get married.</span></li>
-                            </ul>
-                        </div>
+                        <InviteSectionPage />
                     </section>
                 </Container >
             }
